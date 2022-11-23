439a22a740dc7fde3c059e719de83312e794a661
tim <zzz076@yahoo.com.tw>
2022-11-24 02:43:23 +0800

    upload shopping cart

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const portfolioButton = document.getElementById('portfolioBtn');
 const app = document.querySelector('.app');
 
 resumeButton.addEventListener('click', function (event) {
-   window.scrollTo(0,2450);
+   window.scrollTo(0,2920);
   });
 
   portfolioButton.addEventListener('click', function (event) {
